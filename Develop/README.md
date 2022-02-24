c4b5b2d051af1356fd828e28e9040ecd95b3739a
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-02-24 13:48:46 +0000

    created markdown for 3 array questions and tested it and was able to write readme

---
+++ Develop/README.md
@@ -0,0 +1,6 @@
+# read me
+
+<img src = 'https://img.shields.io/badge/license-ISC-blue.svg'>
+
+  ## Description
+read me generator
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'No license') {
+    return `<img src = 'https://img.shields.io/badge/license-${license}-blue.svg'>`
+  }
+  else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +20,8 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
 
   ## Description
 ${data.description}
