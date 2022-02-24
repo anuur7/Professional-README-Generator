6f7b4dbd99bfa5a5c2400ca9a2b50c3fce5a1808
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-02-24 13:38:02 +0000

    Created a function to write README file and finished the init function

--- Develop/index.js
+++ Develop/index.js
@@ -21,11 +21,15 @@ const questions = [{
 }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile('README.md', generateMarkdown({...answers}))
+    })
 }
 
 // Function call to initialize app
