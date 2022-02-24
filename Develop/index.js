ceb16138aaa10b414ff334cf573bed555ce253dc
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-02-24 13:12:43 +0000

    Include packages needed for this application

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const path = require('path')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+}, {
+    type: 'input',
+    name: 'location',
+    message: 'Where are you from?'
+}, {
+    type: 'input',
+    name: 'university',
+    message: 'What do you study?'
+}];
+
+console.log(questions)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
